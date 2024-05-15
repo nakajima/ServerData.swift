2162ebb181bb24dff28ac6dbc93d1f282c1dfd88
Pat Nakajima <patnakajima@gmail.com>
2024-05-14 22:59:16 -0700

    this didn't work

--- Sources/ServerData/Macros.swift
+++ Sources/ServerData/Macros.swift
@@ -11,6 +11,3 @@ public macro Model(table: String) = #externalMacro(module: "ServerDataMacros", t
 
 @attached(peer)
 public macro Column(_ constraints: SQLColumnConstraintAlgorithm..., type: SQLDataType? = nil) = #externalMacro(module: "ServerDataMacros", type: "ColumnMacro")
-
-@freestanding(expression)
-public macro SQLPredicate<each Input: StorableModel>(_ body: (repeat each Input) -> Bool) -> Predicate< repeat each Input> = #externalMacro(module: "FoundationMacros", type: "PredicateMacro")
