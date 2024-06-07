c81616a50b2fc01e6d5929229229b54f312c1143
Pat Nakajima <patnakajima@gmail.com>
2024-06-06 21:53:55 -0700

    bump min ios version

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let swiftSettings: [SwiftSetting] = [
 
 let package = Package(
 	name: "ServerData",
-	platforms: [.macOS(.v14)],
+	platforms: [.macOS(.v14), .iOS(.v17)],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
